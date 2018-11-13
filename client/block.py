2bb4b902b55c7d26eb08d7e04bc93918a445d60c
laochanlam <laochanlam@gmail.com>
2018-11-13 18:46:54 +0800

    modify data structure

--- client/block.py
+++ client/block.py
@@ -5,17 +5,17 @@ import datetime as date
 import hashlib as hasher
 
 class Block(object):
-    def __init__(self, index, timestamp, transactions, previous_hash, proof):
+    def __init__(self, index, timestamp, transactions, previous_hash, nonce):
         self.index = index
         self.timestamp = str(timestamp)
         self.transactions = transactions
         self.pre_hash = previous_hash
-        self.proof = proof
+        self.nonce = nonce
 
     def getHash(self):
         sha = hasher.sha256()
         sha.update(
-            bytes(str(self.index) + str(self.timestamp) + str(self.transactions) + str(self.pre_hash) + str(self.proof),'utf-8')
+            bytes(str(self.index) + str(self.timestamp) + str(self.transactions) + str(self.pre_hash) + str(self.nonce),'utf-8')
         )
         return sha.hexdigest()
 
@@ -25,5 +25,5 @@ class Block(object):
             'timestamp': self.timestamp,
             'transactions': self.transactions,
             'pre_hash': self.pre_hash,
-            'proof': self.proof
+            'nonce': self.nonce
         })
--- client/pool.py
+++ client/pool.py
@@ -1,8 +1,8 @@
 # transaction pool
 class Pool(object):   
     def __init__(self):
-
+        pass
     def push(self,transaction):  # transaction is a dict object
-    
+        pass
     def pop(self):
-    
+        pass
