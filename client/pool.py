9ecf0f8c72f2958b271bf53abb4e48369b184b14
laochanlam <laochanlam@gmail.com>
2018-11-13 20:24:23 +0800

    transaction pool implemented

--- client/pool.py
+++ client/pool.py
@@ -1,8 +1,22 @@
+from sortedcontainers import SortedDict
+import json
+
 # transaction pool
 class Pool(object):   
+    pool = []
+
     def __init__(self):
         pass
-    def push(self,transaction):  # transaction is a dict object
-        pass
+
+    def push(self,transaction):  
+        self.pool.append(transaction)
+        # True stand for success
+        return True
+        
     def pop(self):
-        pass
+        # pop the transaction with largest b_value
+        max_element = max(self.pool, key=lambda x:x['b_value'])
+        return self.pool.pop(self.pool.index(max_element))
+
+    def display(self):
+        return json.dumps(self.pool, indent=4)
--- client/test.py
+++ client/test.py
@@ -1,11 +1,11 @@
-import pool
+from pool import Pool
 
 tx0 = {
     'a_addr': 'test0',
     'a_public_key': 'test0',
     'b_addr': 'test0',
     'b_public_key': 'test0',
-    'a_value': int(10), # refund
+    'a_value': int(10), # refund to A
     'b_value': int(15), # to B
     'unspent': 'test0',
     'signature': 'test0'
@@ -16,7 +16,7 @@ tx1 = {
     'a_public_key': 'test1',
     'b_addr': 'test1',
     'b_public_key': 'test1',
-    'a_value': int(231), # refund
+    'a_value': int(231), # refund to A
     'b_value': int(1), # to B
     'unspent': 'test1',
     'signature': 'test1'
@@ -27,8 +27,18 @@ tx2 = {
     'a_public_key': 'test2',
     'b_addr': 'test2',
     'b_public_key': 'test2',
-    'a_value': int(122), # refund
+    'a_value': int(122), # refund to A
     'b_value': int(12), # to B
     'unspent': 'test2',
     'signature': 'test2'
 }
+
+tx_pool = Pool()
+
+tx_pool.push(tx0)
+tx_pool.push(tx1)
+tx_pool.push(tx2)
+
+print(tx_pool.pop())
+
+print(tx_pool.display())
