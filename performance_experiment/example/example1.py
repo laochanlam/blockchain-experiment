8d5f1fd6d4f0fc158b1aaf2d4ad2e82dd0c409ac
laochanlam <laochanlam@gmail.com>
2018-12-17 09:07:58 +0000

    make example works

--- performance_experiment/example/example1.py
+++ performance_experiment/example/example1.py
@@ -4,25 +4,26 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
- 
+from mininet.node import OVSController
+
 class SingleSwitchTopo(Topo):
-def __init__(self, n=2, **opts):
-Topo.__init__(self, **opts)
-switch = self.addSwitch('s1')     #添加一个交换机在拓扑中
-for h in range(n):
-host = self.addHost('h%s' % (h + 1))   #添加主机到拓扑中
-self.addLink(host, switch)   #添加双向连接拓扑
+    def __init__(self, n=2, **opts):
+        Topo.__init__(self, **opts)
+        switch = self.addSwitch('s1')
+        for h in range(n):
+            host = self.addHost('h%s' % (h + 1))
+            self.addLink(host, switch)  
  
 def simpleTest():
-topo = SingleSwitchTopo(n=4)
-net = Mininet(topo)    #主要类来创建和管理网络
-net.start()    #启动您的拓扑网络
-print "Dumping host connections"
-dumpNodeConnections(net.hosts)       #转存文件连接
-print "Testing network connectivity"     
-net.pingAll()    #所有节点彼此测试互连
-net.stop()       #停止您的网络
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo, controller=OVSController) 
+    net.start() 
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"     
+    net.pingAll()
+    net.stop()
  
 if __name__ == '__main__':
-setLogLevel('info')  # 设置 Mininet 默认输出级别，设置 info 它将提供一些有用的信息
-simpleTest()
+    setLogLevel('info') 
+    simpleTest()
